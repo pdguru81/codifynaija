eda2cb154fc222b5e10dc7ce2d5211955e60676b
philipa <philipa@mit.edu>
2015-06-02 19:31:49 -0400

    committing work

---
+++ codifyNaija/javascript/logic.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+//load facebook plugin
+  $.ajaxSetup({ cache: true });
+  /*$.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+    FB.init({
+      appId: '{your-app-id}',
+      version: 'v2.3' // or v2.0, v2.1, v2.0
+    });     
+    $('#loginbutton,#feedbutton').removeAttr('disabled');
+    FB.getLoginStatus(updateStatusCallback);
+  });*/
+	
+	$("#submit").click(function(){
+
+	});
+
+	$("#").click(function(){
+
+	});
+
+});
